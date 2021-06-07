b2f6af4581eba1d7fc0081379f53273ded0d9011
“Phil <philsmithies@gmail.com>
2021-06-07 17:51:44 +0200

    routes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const connectDB = require('./routes/api/games');
 
 const app = express();
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
 
 const port = process.env.PORT || 8082;
 
