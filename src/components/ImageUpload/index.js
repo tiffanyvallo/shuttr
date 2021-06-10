ae0e16cf4b92a89f9a6cece102f766804a759332
“Phil <philsmithies@gmail.com>
2021-06-10 17:24:57 +0200

    cloud uploads database needed

--- src/components/ImageUpload/index.js
+++ src/components/ImageUpload/index.js
@@ -1,90 +1,34 @@
-// import React from 'react';
-import React, {Component} from 'react'
-import ImageUploading from 'react-images-uploading';
-import axios from 'axios'
+import React, {Component, useState} from 'react'
+import Axios from 'axios'
+import {Image} from 'cloudinary-react'
+import './index.css'
  
-export class ImageUpload extends Component {
-  state = {
-    selectedFile: null
-  }
-  // const [images, setImages] = React.useState([]);
-  // const maxNumber = 69;
-  // const onChange = (imageList, addUpdateindex) => {
-  //   // data for submit
-  //   console.log(imageList, addUpdateindex);
-  //   setImages(imageList);
-  // };
-  
-  // function handleSubmit(e) {
-  //   e.preventDefault()
-  //   console.log('You clicked submit.');
-  // }
-  fileSelectedHandler = event => {
-    this.setState({
-      selectedFile: event.target.files[0]
+function ImageUpload() {
+  const [hashtag, setHashtag] = useState("");
+  const [caption, setCaption] = useState("");
+  const [imageSelected, setImageSelected] = useState("")
+
+  const uploadImage = () => {
+    console.log(imageSelected)
+    const formData = new FormData()
+    formData.append("file", imageSelected)
+    formData.append("upload_preset", "cyber_photos")
+
+    Axios.post("https://api.cloudinary.com/v1_1/dryaxqxie/image/upload", formData).then((response) => {
+      console.log(response)
     })
   }
 
-  fileUploadHandler = () => {
-    const fd = new FormData();
-    fd.append('image', this.state.selectedFile, this.state.selectedFile.name)
-    console.log(fd)
-    // app.post('/photos', fd)
-    // .then(function (response){
-    //   console.log(response)
-    // })
-  }
-
- render(){
   return (
-    <div className="App">
-      <input type="file" onChange={this.fileSelectedHandler}></input>
-      <button onClick={this.fileUploadHandler}>Upload</button>
-      {/* <ImageUploading
-        multiple
-        value={images}
-        onChange={onChange}
-        maxNumber={maxNumber}
-        acceptType={['jpg', 'gif', 'png']}
-        dataURLKey="data_url"
-      >
-        {({
-          imageList,
-          onImageUpload,
-          onImageRemoveAll,
-          onImageUpdate,
-          onImageRemove,
-          isDragging,
-          dragProps,
-        }) => (
-          // write your building UI
-          <div className="upload__image-wrapper">
-            <button
-              style={isDragging ? { color: 'red' } : undefined}
-              onClick={onImageUpload}
-              {...dragProps}
-            >
-              Click or Drop here
-            </button>
-            &nbsp;
-            <button onClick={onImageRemoveAll}>Remove all images</button>
-            {imageList.map((image, index) => (
-              <div key={index} className="image-item">
-                <img src={image['data_url']} alt="" width="500" />
-                <div className="image-item__btn-wrapper">
-                  <button onClick={() => onImageUpdate(index)}>Update</button>
-                  <button onClick={() => onImageRemove(index)}>Remove</button>
-                  <form onSubmit={handleSubmit}>
-                    <button type="submit">Submit</button>
-                 </form>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </ImageUploading> */}
+    <div className="form_wrapper">
+      <input type="file" onChange={(event) => {setImageSelected(event.target.files[0])}}/>
+      <input type="text" placeholder="hashtag" />
+      <input type="text" placeholder="caption" />
+      <button onClick={uploadImage}>Upload Image</button>
+
+      <Image className="cloud_photo" cloudName="cyber_photos" publicId="https://res.cloudinary.com/dryaxqxie/image/upload/v1623337463/jckg0zqclgbkitlzv9uv.jpg"/> 
     </div>
-  );
-}}
+  )
+}
 
 export default ImageUpload
