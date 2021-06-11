6722eaa0a0ee16aca3d739366d4442c27d4cebe4
“Phil <philsmithies@gmail.com>
2021-06-11 12:04:46 +0200

    photo upload rewrite

--- src/components/ImageUpload/index.js
+++ src/components/ImageUpload/index.js
@@ -6,26 +6,67 @@ import './index.css'
 function ImageUpload() {
   const [hashtag, setHashtag] = useState("");
   const [caption, setCaption] = useState("");
-  const [imageSelected, setImageSelected] = useState("")
+  const [selectedFile, setSelectedFile] = useState("")
+  const [fileInputState, setFileInputState] = useState("")
+  const [previewSource, setPreviewSource] = useState("")
 
-  const uploadImage = () => {
-    console.log(imageSelected)
-    const formData = new FormData()
-    formData.append("file", imageSelected)
-    formData.append("upload_preset", "cyber_photos")
+  // if file changes then change state on everything
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0]
+    previewFile(file)
+    setSelectedFile(file);
+    setFileInputState(e.target.value);
+  }
 
-    Axios.post("https://api.cloudinary.com/v1_1/dryaxqxie/image/upload", formData).then((response) => {
-      console.log(response)
-    })
+  const previewFile = (file => {
+    const reader = new FileReader();
+    // reads the file as url to create preview
+    reader.readAsDataURL(file)
+    reader.onloadend = () => {
+      setPreviewSource(reader.result)
+    }
+  })
+
+  // handles the submit and prevents the page reloading then pushes it online
+  const handleSubmitFile = (e) => {
+    e.preventDefault();
+    if(!previewSource) return;
+    const reader = new FileReader()
+    uploadImage(previewSource)
+  }
+   // turns the image into a string then does async request to push could change to axios
+  const uploadImage = async (base64EncodedImage) => {
+    console.log(base64EncodedImage)
+    try {
+      await fetch('/api/upload', {
+        method: 'POST',
+        body: JSON.stringify({data: base64EncodedImage}),
+        headers: {'Content-type': 'application/json'}
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <div className="form_wrapper">
-      <input type="file" onChange={(event) => {setImageSelected(event.target.files[0])}}/>
       <input type="text" placeholder="hashtag" />
       <input type="text" placeholder="caption" />
-      <button onClick={uploadImage}>Upload Image</button>
-
+      <form onSubmit={handleSubmitFile}>
+                <input
+                    id="fileInput"
+                    type="file"
+                    name="image"
+                    onChange={handleFileInputChange}
+                    value={fileInputState}
+                />
+                <button type="submit">
+                    Submit
+                </button>
+      </form>
+      {previewSource && (
+        <img src={previewSource} alt="chosen" style={{height: '300px'}}/>
+      )}
       <Image className="cloud_photo" cloudName="cyber_photos" publicId="https://res.cloudinary.com/dryaxqxie/image/upload/v1623337463/jckg0zqclgbkitlzv9uv.jpg"/> 
     </div>
   )
