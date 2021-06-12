b993e58b4057c6fc6f4af9579c327862c06424d5
Adam <adam.j.mccor@gmail.com>
2021-06-12 13:54:56 +0100

    'add mapbox map and default to London'

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -5,16 +5,17 @@ import { Link } from 'react-router-dom';
 
 function Map() {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    width: "100vw",
+    height: "100vh",
+    latitude: 51.5074,
+    longitude: 0.1278,
+    zoom: 10
   });
   return (
     <div>
        <ReactMapGL
       {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
       onViewportChange={nextViewport => setViewport(nextViewport)}
     />
     </div>
