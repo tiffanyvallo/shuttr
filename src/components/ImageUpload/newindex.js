4395441f0c941d2809c77ebe549e1acefc41c3b2
“Phil <philsmithies@gmail.com>
2021-06-12 17:57:36 +0200

    cloud photos working

--- src/components/ImageUpload/newindex.js
+++ src/components/ImageUpload/newindex.js
@@ -8,6 +8,7 @@ export default function ImageUpload() {
   const preset = 'cyber_photos';
   const [image, setImage] = useState('');
   const [hashtag, setHashtag] = useState("");
+  const [location, setLocation] = useState("");
   const [caption, setCaption] = useState("");
   const [loading, setLoading] = useState(false);
   const onChange = e => {
@@ -38,8 +39,9 @@ export default function ImageUpload() {
     <div className="form_wrapper">
         <input type='file' name='image' onChange={onChange} />
         <input type="text"  onChange={(e) => setHashtag(e.target.value)} value={hashtag} placeholder="hashtag" />
+        <input type="text"  onChange={(e) => setLocation(e.target.value)} value={location} placeholder="location" />
         <input type="text" onChange={(e) => setCaption(e.target.value)} value={caption} placeholder="caption" />
-        <button onClick={onSubmit} className='btn center'>
+        <button onClick={onSubmit}>
           Upload
         </button>
         <Image className="cloud_photo" cloudName="cyber_photos" publicId="https://res.cloudinary.com/dryaxqxie/image/upload/v1623337463/jckg0zqclgbkitlzv9uv.jpg"/> 
