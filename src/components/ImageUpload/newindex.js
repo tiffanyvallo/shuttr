357d04fe79dcd41c6d766f9a9f06c45baaba247c
“Phil <philsmithies@gmail.com>
2021-06-13 17:33:27 +0200

    hashtags drop down

--- src/components/ImageUpload/newindex.js
+++ src/components/ImageUpload/newindex.js
@@ -3,8 +3,25 @@ import {Image} from 'cloudinary-react'
 import axios from 'axios';
 import './index.css'
 import loadingGif from './Loading_icon.gif';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 export default function ImageUpload() {
+  const classes = useStyles();
   const url = 'https://api.cloudinary.com/v1_1/dryaxqxie/image/upload';
   const preset = 'cyber_photos';
   const [image, setImage] = useState('');
@@ -66,7 +83,23 @@ export default function ImageUpload() {
     <div className="form_wrapper">
         {isLoading()}
         <input type='file' name='image' onChange={onChange} />
-        <input type="text"  onChange={(e) => setHashtag(e.target.value)} value={hashtag} placeholder="hashtag" />
+        <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-outlined">Hashtag</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={hashtag}
+          onChange={(e) => setHashtag(e.target.value)}
+        >
+          <MenuItem value={"Abstract"}>Abstract</MenuItem>
+          <MenuItem value={"Colourful"}>Colourful</MenuItem>
+          <MenuItem value={"Rustic"}>Rustic</MenuItem>
+          <MenuItem value={"Weird"}>Rustic</MenuItem>
+          <MenuItem value={"Pubs"}>Pubs</MenuItem>
+          <MenuItem value={"Architecture"}>Architecture</MenuItem>
+        </Select>
+      </FormControl>
+        {/* <input type="text"  onChange={(e) => setHashtag(e.target.value)} value={hashtag} placeholder="hashtag" /> */}
         <input type="text"  onChange={(e) => setLocation(e.target.value)} value={location} placeholder="location" />
         <input type="text" onChange={(e) => setCaption(e.target.value)} value={caption} placeholder="caption" />
         <button onClick={onSubmit}>
