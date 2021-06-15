cd2d28d0015c55105b69d7a0c9cdf1275d048251
Adam <adam.j.mccor@gmail.com>
2021-06-15 12:15:52 +0100

    'add db pins to map'

--- src/Pages/Map/index.jsx
+++ src/Pages/Map/index.jsx
@@ -1,10 +1,27 @@
 import * as React from 'react';
-import { useState } from 'react';
-import ReactMapGL, {Marker, Popup} from 'react-map-gl';
-import {Room} from "@material-ui/icons"
+import { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { Room } from "@material-ui/icons"
 import { Link } from 'react-router-dom';
+import axios from "axios";
+import {Image} from 'cloudinary-react';
+
 
 function Map() {
+
+  const [allData, setAllData] = useState([]);
+
+  useEffect(() => {
+    axios("http://localhost:3001/photos")
+      .then((response) => {
+        console.log(response.data);
+        setAllData(response.data);
+      })
+      .catch((error) => {
+        console.log("Error getting data: " + error);
+      });
+  }, []);
+
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -20,9 +37,30 @@ function Map() {
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
       onViewportChange={nextViewport => setViewport(nextViewport)}
       mapStyle="mapbox://styles/ajmccor/ckpttmayl2xwn18pg496rt567"
-    >
+    >   
+
+        <div>
+        { allData.map((value,index) => { 
+          return (
+            <div key={value.id}>
+             
+             <Marker 
+                latitude={(value.coordinates || {lat: 0}).lat}
+                longitude={(value.coordinates || {lng: 0}).lng}
+                offsetLeft={-20} 
+                offsetTop={-10}>
+
+                <Room style={{fontSize:viewport.zoom * 4, color:"orangeRed"}}/>
+        
+              </Marker>
+             {/* <Image className="cloud_photo" cloudName="cyber_photos" publicId={value.publicId} /> */}
+            </div>
+          );
+         })}
+        </div>
+
       <Marker latitude={51.50930532431084}
-              longitude={-0.12829965320115588} 
+              longitude={-0.12829965320115588}
               offsetLeft={-20} 
               offsetTop={-10}>
 
