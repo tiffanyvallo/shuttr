6098045a07c9ec780bf17f3602944fa35005334d
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-06-17 22:17:20 +0100

    color change

--- src/Pages/Discover/index.jsx
+++ src/Pages/Discover/index.jsx
@@ -77,6 +77,7 @@ export default function TitlebarGridList() {
                   <p>#{value.hashtag}</p>
                   <p>Taken at: {value.location}</p>
                  <a href={'/profile/' + value.author}> @{value.author}</a>
+                 <br/>
                   <Popup
     trigger={<button className="button">View More</button>}
     modal
