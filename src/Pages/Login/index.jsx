7e32e0e5fc933154ef3fc5b637ebc5cda67a35c2
Tiago Manuel <79860571+TiagoManuelPC@users.noreply.github.com>
2021-06-17 19:47:28 +0100

    log in form

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -2,14 +2,73 @@ import React, {useState} from "react";
 import Axios from "axios";
 import './index.css'
 import { Link } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+// import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      {/* <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '} */}
+      Shuttr 
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1), 
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 
 
 export default function Login() {
   const [loginUsername, setLoginUsername] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
-  const signup = () =>{
-    window.location.href = "/"; 
-  }
+  // const signup = () =>{
+  //   window.location.href = "/"; 
+  // }
    const login = () => {
     
     Axios({     
@@ -20,44 +79,120 @@ export default function Login() {
       },      withCredentials: true,      
       url: "http://localhost:3001/login",    
       }).then((res) => {      
-      if (res.data === "Successfully Authenticated") {
+      // if (res.data === "Successfully Authenticated") {
         window.location.href = "/discover";      
-      }    
+      // }    
     });  
   };
-  
+  const classes = useStyles();
   return (
-    <div class="login_wrapper">
-      <h1>Log in here</h1>
+    <Grid container component="main" className={classes.root}>
+    <CssBaseline />
+    <Grid item xs={false} sm={4} md={7} className={classes.image} />
+    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="User Name"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={(e) => {
+             setLoginUsername(e.target.value);
+            }}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e) => {
+            setLoginPassword(e.target.value);
+            }}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            // className={classes.submit}
+            onClick={login}
+            href = "/discover"
+          >
+            Login
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              {/* <Link href="/inspiration" variant="body2">
+                Forgot password?
+              </Link> */}
+            </Grid>
+            <Grid item>
+              <Link to="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+          <Box mt={5}>
+            <Copyright />
+          </Box>
+        </form>
+      </div>
+    </Grid>
+  </Grid>
+
+//-------------------------------------------------------------------
+    // <div class="login_wrapper">
+    //   <h1>Log in here</h1>
       
 
-        <input 
-        type="text" 
-        placeholder="Username"
-        onChange={(e) => {
-          setLoginUsername(e.target.value);
-          }}
-        />
+    //     <input 
+    //     type="text" 
+    //     placeholder="Username"
+    //     onChange={(e) => {
+    //       setLoginUsername(e.target.value);
+    //       }}
+    //     />
      
       
-        <input 
-        type="password"
-        placeholder="Password"
-        onChange={(e) => {
-          setLoginPassword(e.target.value);
-          }} />
+    //     <input 
+    //     type="password"
+    //     placeholder="Password"
+    //     onChange={(e) => {
+    //       setLoginPassword(e.target.value);
+    //       }} />
       
-      <div>
-        <button type="submit" onClick={login}>Log In</button>
-      </div>
-      <div >
-      <br />
-        Don't have an account? <Link to="/signup" className="btn btn-primary">   
-        <br />               
-      <button>Sign Up</button>   
-      </Link> 
-      </div>
-    </div>
+    //   <div>
+    //     <button type="submit" onClick={login}>Log In</button>
+    //   </div>
+    //   <div >
+    //   <br />
+    //     Don't have an account? <Link to="/signup" className="btn btn-primary">   
+    //     <br />               
+    //   <button>Sign Up</button>   
+    //   </Link> 
+    //   </div>
+    // </div>
     )
 }
 
