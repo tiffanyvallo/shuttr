d00243240ba9a0c6425cae6b0d6ec815a7c664d6
“Phil <philsmithies@gmail.com>
2021-06-18 06:41:56 +0200

    iphone

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,17 +3,22 @@ import { Image } from 'cloudinary-react'
 import './index.css'
 import drone from './banner_dark.mp4';
 import logo from './shuttrlogo.png'
+import shuttrIphone from './shuttr_iphone.png'
 
 export default function Home() {
   return (
     <div class="home-wrapper">
       <div class="fullscrenvideo">
-      <div class="overlay"><img class="shuttrlogo" src={logo}></img></div>
+      {/* <div class="overlay"><img class="shuttrlogo" src={logo}></img></div> */}
       <video className='videoTag' autoPlay loop muted>
             <source src={drone} type='video/mp4' />
       </video>
       </div>
-      <div class="cta-wrapper"></div>
+      <div class="cta-wrapper">
+      <img class="shuttriphone" src={shuttrIphone}></img>
+
+
+      </div>
       <div class="grid">
         <div class="grid-left">
           <p>Hey this is the CTA FORM LOOK HOW COOL OUR SITE IS</p>
